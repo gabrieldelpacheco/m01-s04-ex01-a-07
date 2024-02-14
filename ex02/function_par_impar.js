39d3dd6a0c9bddbe6dc918acf74762e9ed4f3bfe
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2024-02-14 10:26:57 -0300

    exercicios 1 e 2

---
+++ ex02/function_par_impar.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+function parImpar(numero) {
+    let resultado = numero % 2;
+    if (resultado === 0) {
+        console.log(`O número informado ${numero} é par.`);
+    } else {
+        console.log(`O número informado ${numero} é impar.`);
+    }
+}
+
+console.log("Descubra se o número é PAR ou IMPAR.");
+let numero = Number(prompt("Informe um número: "));
+
+parImpar(numero);
